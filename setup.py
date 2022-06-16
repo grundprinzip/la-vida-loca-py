f4f6e1ba7500870cf6598e60e82775eb47186e41
Martin Grund <martin.grund@databricks.com>
2022-06-16 20:37:47 +0200

    Update

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='la-vida-local',
     version='1.0',
-    packages=['.pyspark'],
+    packages=['.pyspark', '.pyspark.sql', '.pyspark.sql.connect', '.pyspark.sql.connect.proto'],
 )
